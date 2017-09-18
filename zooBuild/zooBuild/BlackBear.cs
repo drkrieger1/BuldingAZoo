4a318a44f5448aacd762316ab379b4e7db8b74b0
drkrieger <eplyushko@gmail.com>
2017-09-17 19:55:26 -0700

    zoo build

--- zooBuild/zooBuild/BlackBear.cs
+++ zooBuild/zooBuild/BlackBear.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace zooBuild
 {
-    class BlackBear
+    class BlackBear : Ursidae
     {
+        public override void Eat (string food)
+        {
+            Console.WriteLine($"Black bears eat {food}"); 
+        }
+
+        public override bool Hair()
+        {
+            return true;
+        }
+        public override void Hairtype()
+        {
+            Console.WriteLine("shaggy");
+        }
     }
 }
--- zooBuild/zooBuild/Program.cs
+++ zooBuild/zooBuild/Program.cs
@@ -6,7 +6,26 @@ namespace zooBuild
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my zoo");
+
+            BlackBear bill = new BlackBear();
+            bill.Location = "Seattle";
+            bill.Handler = "Erik";
+            bill.HairColor = "black";
+            bill.Cage = "big cage";
+
+            Console.WriteLine($"Bill is a Black Bear that lives in a zoo located in {bill.Location}");
+            Console.WriteLine($"Bills handler is {bill.Handler}");
+            Console.WriteLine($"Bills fur color is {bill.HairColor} and he has a {bill.Cage}");
+            Console.WriteLine($"Bill likes to:");
+            bill.Eat("meat");
+            bill.Sleep();
+            Console.WriteLine($"Bills fur is");
+            bill.Hairtype();
+            Console.WriteLine($"Black bears are mammals because they:");
+            bill.GiveBirth();
+
+
             Console.Read();
         }
     }
